2c20890766810d13c98044da7d1c34ac1d780618
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-06-04 12:35:18 -0700

    setup borders for the road

--- road.js
+++ road.js
@@ -11,6 +11,20 @@ class Road{
         const infinity = 1000000;
         this.top = -infinity;
         this.bottom = infinity;
+
+        //create road points for border segments
+        const topLeft={x:this.left, y:this.top};
+        const topRight={x:this.right, y:this.top};
+        const bottomLeft={x:this.left, y:this.bottom};
+        const bottomRight={x:this.right, y:this.bottom};
+
+        //create borders for the car
+        // use multiple arrays to create border segments of two points
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ];
+
     }
 
     getLaneCenter(laneIdx){
@@ -26,8 +40,8 @@ class Road{
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
         
-        // use linear interpolation to get the x placement of the lanes
-        for(let i=0; i<=this.laneCount;i++){
+        // use linear interpolation to get the x placement of the inner lanes
+        for(let i=1; i<=this.laneCount-1;i++){
             // from left to write, using percentage based on total lanes
             const x = lerp(
                 this.left,
@@ -35,14 +49,23 @@ class Road{
                 i/this.laneCount
             );
 
-            // draw the lanes of the road, add dashes if they are middle names
-            (i>0 && i<this.laneCount) ? ctx.setLineDash([20,20]) : ctx.setLineDash([]);
+            // draw the middle lanes of the road
+            ctx.setLineDash([20,20]);
 
             ctx.beginPath();
-            ctx.moveTo(x, this.top);
-            ctx.lineTo(x, this.bottom);
+            ctx.moveTo(x, this.top); //sets starting point for the draw
+            ctx.lineTo(x, this.bottom); // draws a line from starting point to given point
             ctx.stroke();
         }
 
+        // draw the game borders
+
+        ctx.setLineDash([]);
+        this.borders.forEach(border => {
+            ctx.beginPath();
+            ctx.moveTo(border[0].x,border[0].y);
+            ctx.lineTo(border[1].x,border[1].y);
+            ctx.stroke();
+        })
     }
 }
