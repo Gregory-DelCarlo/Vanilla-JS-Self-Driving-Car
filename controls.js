81458236f2cc56fe48951049b2d5f48265c5520f
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-06-04 10:10:23 -0700

    added basic car controls with key events

---
+++ controls.js
@@ -0,0 +1,49 @@
+class Controls {
+    constructor() {
+        this.forward = false;
+        this.backward = false;
+        this.left = false;
+        this.right = false;
+
+        this.#addKeyboardListeners();
+    }
+
+    // create private method to listen for keyboard input
+
+    #addKeyboardListeners(){
+        // use arrow function here to avoid inheritance issues
+        document.onkeydown=(event)=>{
+            switch(event.key) {
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.backward = true;
+                    break;
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+            }
+        }
+        // stops input when key is released
+        document.onkeyup=(event)=>{
+            switch(event.key) {
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.backward = false;
+                    break;
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+            }
+        }
+    }
+}
