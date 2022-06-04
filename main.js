a70ef6df978cd10cad231d0567846e5d4d00c396
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-06-04 10:18:42 -0700

    fix canvas to update along with car position

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // searches the html doc its on for an element with id #mainCanvas
 const canvas = document.getElementById("mainCanvas");
 // window refers to the size of the browser
-canvas.height = window.innerHeight;
 canvas.width=200;
 
 
@@ -12,6 +11,7 @@ animate();
 
 function animate(){
     car.update();
+    canvas.height = window.innerHeight;
     car.draw(ctx);
     // calls this method everytime it finishes
     // allows the illusion of movement
