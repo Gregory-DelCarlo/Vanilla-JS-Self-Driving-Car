acb9a9b6f219195375e3b68f6d9bce70f61ea3ca
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-06-04 10:37:50 -0700

    Implemented basic phyics for more realistic car movement

--- car.js
+++ car.js
@@ -6,16 +6,43 @@ class Car{
         this.width = width;
         this.height = height;
 
+        // instead of directly mainpulating the position of the car we can use speed and acceleration to create
+        // the illusion of inertia and friction
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+
         this.controls = new Controls();
     }
 
     update(){
         // on webpages (0,0) is the top left corner so moving -2 in y moves the car up the page
         if(this.controls.forward){
-            this.y-=2;
+            this.speed += this.acceleration;
         }else if(this.controls.backward){
-            this.y+=2;
+            this.speed -= this.acceleration;
         }
+
+        // cap the speed of the car
+        if (this.speed>this.maxSpeed){
+            this.speed = this.maxSpeed;
+        } else if (this.speed< -this.maxSpeed/2){
+            this.speed = -this.maxSpeed/2;
+        }
+
+        // simulates friction this will cause the car to decelerate when a direction is not being pressed
+        if (this.speed>0){
+            this.speed -= this.friction;
+        }else if (this.speed<0){
+            this.speed += this.friction;
+        }
+
+        if(Math.abs(this.speed)<this.friction) {
+            this.speed = 0;
+        }
+        // update y position based on speed (in px) of the car 
+        this.y -= this.speed;
     }
 
     draw(ctx){
