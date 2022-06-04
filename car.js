b627dc6df1b7abfe445c9ef12b8ecde9e514c9a5
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-06-04 09:57:42 -0700

    got a car ref to render

--- car.js
+++ car.js
@@ -1,5 +1,20 @@
 class Car{
-    constructor(){
+    constructor(x,y,width,height){
+        // stores information about itself (position, and size)
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
 
+    draw(ctx){
+        ctx.beginPath();
+        ctx.fillRect(
+            //sets the origin points to the center of the rectangle's size
+            this.x-this.width/2,
+            this.y-this.height/2,
+            this.width,
+            this.height
+        );
     }
 }
--- main.js
+++ main.js
@@ -7,22 +7,6 @@ canvas.width=200;
 
 const ctx = canvas.getContext("2d");
 const car = new Car(100,100,30,50);
-// car.draw(ctx);
-
-ctx.lineWidth = 10;
-
-// Wall
-ctx.strokeRect(75, 140, 150, 110);
-
-// Door
-ctx.fillRect(130, 190, 40, 60);
-
-// Roof
-ctx.beginPath();
-ctx.moveTo(50, 140);
-ctx.lineTo(150, 60);
-ctx.lineTo(250, 140);
-ctx.closePath();
-ctx.stroke();
+car.draw(ctx);
 
 
