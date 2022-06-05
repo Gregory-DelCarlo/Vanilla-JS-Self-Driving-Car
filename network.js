c6489abdbdabc78f26868a3564396b53df486d9b
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-06-05 06:58:52 -0700

    Added structure of the Neural Network

--- network.js
+++ network.js
@@ -1,3 +1,35 @@
+class NeuralNetwork{
+    constructor(neuronCounts){ // an array of the amount of neurons each level of the network has
+        this.levels=[];
+
+        for(let i =0; i< neuronCounts.length; i++){ // creates a new level's inputs, and connects the next level to it
+            this.levels.push(new Level(
+                neuronCounts[i], neuronCounts[i+1]
+            ));
+        }
+    }
+
+
+    // feed forward algorithms move the information through the neural network
+    // this feed forward concerns itself with the network as a whole and uses Level's feed forward
+    // to contain the calculations within the level itself
+    static feedForward(givenInputs, network){ //when given a set of inputs and a network
+        let outputs = Level.feedForward(  // calls the first level of the network to produce it's outputs
+            givenInputs, network.levels[0] 
+        );
+
+        for(let i=1;i<network.levels.length;i++){ // starting with the previously calculated inputs
+            outputs = Level.feedForward(          // get the outputs for each consecutive level
+                outputs, network.levels[i]
+            );
+        }
+
+        return outputs;  // return the final output
+    }
+}
+
+
+
 class Level{  // controls one level of our nerual network
     constructor(inputCount, outputCount){
         this.inputs = new Array(inputCount);
@@ -9,7 +41,7 @@ class Level{  // controls one level of our nerual network
 
         // creates a connection from each input node to all the output nodes
         for(let i=0;i<inputCount; i++){
-            this.weights[i] = new Array(outputCount);
+            this.weights[i] = new Array(outputCount);``
         }
 
         Level.#randomize(this); //randomize the weights and biases on this level
@@ -26,7 +58,8 @@ class Level{  // controls one level of our nerual network
         level.biases.forEach(bias => ( bias = Math.random()*2-1 ));
     }
 
-    // compute output values using this algorithmn
+    // compute output values using this algorithmn, this feed forward only concerns itself with the interaction
+    // of the current level to the next level
     static feedForward(givenInputs, level){
         // receive inputs from sensors
         for(let i=0; i<level.inputs.length;i++){
