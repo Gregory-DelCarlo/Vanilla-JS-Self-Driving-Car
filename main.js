dcf581a79cceb9a0db199653197b775d51fabbad
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-06-14 04:41:45 -0700

    Added Simple Parrelization

--- main.js
+++ main.js
@@ -12,36 +12,55 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
-const car = new Car(road.getLaneCenter(1),400,40,60, "AI"); //need to specify between npc and pc cars
+const cars = generateCars(100);
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 40, 60, "NPC", 2),
 ]  //eventually I want to update this to be all cars and deal with traffic within cars
    // this should allow npc cars to crash into eah other and into the pc
+   
 
 animate();
 
-function animate(){
+function generateCars(genSize){
+    const cars = [];
+
+    for(let i=0; i<genSize; i++){
+        cars.push(new Car(road.getLaneCenter(1),400,40,60, "AI"));
+    }
+
+    return cars;
+}
+
+function animate(time){
     for(i=0;i<traffic.length;i++){
         traffic[i].update(road.borders, []); // updates cars in traffic, dont pass npc cars traffic for collisions
     }
 
-    car.update(road.borders, traffic);
+    for(let i=0; i<cars.length;i++){
+        cars[i].update(road.borders, traffic);
+    }
+
     carCanvas.height = window.innerHeight;
     networkCanvas.height = window.innerHeight;
 
     // adds camera like effect to follow the car
     carCtx.save();
-    carCtx.translate(0, -car.y+carCanvas.height*0.7);
+    carCtx.translate(0, -cars[0].y+carCanvas.height*0.7); // only follow one car
 
     road.draw(carCtx);
     for(i=0;i<traffic.length;i++){
         traffic[i].draw(carCtx);
     }
-    car.draw(carCtx);
+    carCtx.globalAlpha=0.2;
+    for(i=0; i<cars.length;i++){
+        cars[i].draw(carCtx);
+    }
+    carCtx.globalAlpha=1;
 
     carCtx.restore();
 
-    Visualizer.drawNetwork(networkCtx, car.brain);  //method call to properly visualize the given network
+    networkCtx.lineDashOffset=-time/50; 
+    Visualizer.drawNetwork(networkCtx, cars[0].brain);  //method call to properly visualize the given network
     // calls this method everytime it finishes
     // allows the illusion of movement
     requestAnimationFrame(animate);
